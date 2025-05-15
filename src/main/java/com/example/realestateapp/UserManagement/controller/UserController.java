8715e071e584711e41ef27fe561b26c83756ecc0
Vohara DE Silva <IT24101306>
2025-05-15 13:43:57 +0530

    update

---
+++ src/main/java/com/example/realestateapp/UserManagement/controller/UserController.java
@@ -0,0 +1,8 @@
+package com.example.realestateapp.UserManagement.controller;
+
+
+public class UserController {
+    public static void main (String[] args){
+        System.out.println("Testing...");
+    }
+}
