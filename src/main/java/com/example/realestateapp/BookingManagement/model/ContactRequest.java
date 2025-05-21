25587b5de0c85fa9073ccbe9facaf5ca4521094d
IT24101071 <it24101071@my.sliit.lk>
2025-05-22 05:16:46 +0530

    Booking Management - Version 8

--- src/main/java/com/example/realestateapp/BookingManagement/model/ContactRequest.java
+++ src/main/java/com/example/realestateapp/BookingManagement/model/ContactRequest.java
@@ -1,9 +1,10 @@
 package com.example.realestateapp.BookingManagement.model;
 
-public class ContactRequest {
-    private String visitorName;
-    private String date;
-    private String timeSlot;
+import lombok.Data;
+@Data
+
+
+public class ContactRequest extends Request {
     private String contactNumber;
     private String message;
     private String propertyLocation;
@@ -11,25 +12,11 @@ public class ContactRequest {
     private boolean isRead;
 
     public ContactRequest(String visitorName, String date, String timeSlot, String contactNumber, String message, String propertyLocation, String owner, boolean isRead) {
-        this.visitorName = visitorName;
-        this.date = date;
-        this.timeSlot = timeSlot;
+        super(visitorName, date, timeSlot);
         this.contactNumber = contactNumber;
         this.message = message;
         this.propertyLocation = propertyLocation;
         this.owner = owner;
         this.isRead = isRead;
     }
-
-
-    public String getVisitorName() { return visitorName; }
-    public String getPropertyLocation() { return propertyLocation; }
-    public String getDate() { return date; }
-    public String getTimeSlot() { return timeSlot; }
-    public String getContactNumber() { return contactNumber; }
-    public String getMessage() { return message; }
-    public String getOwner() { return owner; }
-    public boolean isRead() { return isRead; }
-    public void setRead(boolean read) { isRead = read; }
-
 }
